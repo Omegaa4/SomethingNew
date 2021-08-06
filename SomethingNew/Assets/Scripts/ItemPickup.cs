2f98f58cd7cd41fd87be520ea1cbc9d32e0501d2
lexHOME <66259@selwyn.school.nz>
2021-08-07 01:58:28 +1200

    Inventory + Placeholder Item
    +inventory slot ui
    +slots can take items
    +bug where the duplicated slots are weird
    +placeholder lightning item that goes into a slot when touched
    +various art assets

---
+++ SomethingNew/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script was adapted from the 2018 blackthornprod inventory tutorial, and was written by Alexander
+public class ItemPickup : MonoBehaviour
+{
+    private Inventory inventory; //Private variable called inventory that references the Inventory Class
+    public GameObject lightningButton; //Public game object variable called ligtningButton
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //Sets the inventory variable as the Inventory component of the GameObject with the 'player' tag
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>(); 
+    }
+
+    void OnTriggerEnter2D(Collider2D other) //Method with a parameter for a 2D Collider object
+    {
+        if (other.CompareTag("Player")) //Check if other has the 'player' tag
+        {
+            for (int i = 0; i < inventory.slots.Length; i++) //For loop that runs through the slots array, stopping when i is not less than the slots array length
+            {
+                if (inventory.isFull[i] == false) //Checks if the slot we are currently on is empty
+                {
+                    inventory.isFull[i] = true; //If the slot is empty then set it as full
+                    Instantiate(lightningButton, inventory.slots[i].transform, false); //Creates a clone of lightningButton, parents it to the slot chosen which also puts it in the same position as the slot, specifies not in world space
+                    Destroy(gameObject); //Destroys the item as the player has 'picked' it up
+                    break; //Get out of the for loop
+                }
+            }         
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
