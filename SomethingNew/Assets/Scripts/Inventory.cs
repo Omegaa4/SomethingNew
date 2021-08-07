3f2a3fef36246751747033570e025963b54cdbe4
lexHOME <66259@selwyn.school.nz>
2021-08-07 22:16:05 +1200

    Revert "Inventory + Placeholder Item"
    This reverts commit 2f98f58cd7cd41fd87be520ea1cbc9d32e0501d2.

--- SomethingNew/Assets/Scripts/Inventory.cs
+++ SomethingNew/Assets/Scripts/Inventory.cs
@@ -2,25 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//This script was made by Alexander, modified from the 28 Aug 2018 blackthornprod inventory tutorial
-public class Inventory : MonoBehaviour
+//This script was made by Alexander Yong, modified from the 28 Aug 2018 blackthornprod inventory tutorial
+public class NewBehaviourScript : MonoBehaviour
 {
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
     public bool[] isFull;       //A boolean array that is used to check if an inventory slot is full or empty
     public GameObject[] slots; //Array for referencing the slots 
     
-    
+
 }
