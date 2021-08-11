6ed4cf17262aae0195897cda01e733225f70fb82
AlexTheBowlerHat <85088627+AlexTheBowlerHat@users.noreply.github.com>
2021-08-12 10:41:10 +1200

    Spawn script + wrote more in SlotRefresh
    title
    renamed drop script

---
+++ SomethingNew/Assets/Scripts/SlotRefresh.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//This script was made by Alexander, following the August 2018 blackthornprod tutorial
+public class SlotRefresh : MonoBehaviour
+{   
+    private Inventory inventory; //Inventory component variable named variable
+    public int i; //An empty integer
+
+// Start is called before the first frame update
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>(); //Finds the component of the inventory type from a game object with a player tag and assigns it to the inventory variable
+    }
+
+// Update is called once per frame
+    private void Update()
+    {
+        if (transform.childCount <= 0) //Checks how many children the game object the script is attached to has
+        {
+            inventory.isFull[i] = false; //Sets the index of i in the isFull array to false
+        }
+    }
+
+    //Method that is called when the slot's button is presssed
+    public void DropItem()
+    {
+        foreach (Transform child in transform) //Foreach loop that runs for the number of children of the slot
+        {
+            GameObject.Destroy(child.gameObject); //Deletes the cloned objects
+        }
+    }
+
+}
---
+++ SomethingNew/Assets/Scripts/Spawn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//This script wass created by Alexander, modified from the blackthornprod 2018 inventory tutorial
+public class Spawn : MonoBehaviour
+{
+    public GameObject item; //Variable of the GameObject type 
+    private Transform playerTransform; //Transform component variable
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform; //Sets the player variable to the transform property of the game object with the player tag
+    }
+
+    //Called when the cross linked with the slot is clicked, to simulate dropping the item
+    public void SpawnDroppedItem()
+    {
+        Vector2 playerPos = new Vector2(playerTransform.position.x + 3, playerTransform.position.y); //Vector 2 that contains the player object's x position + 3 and the y position
+        Instantiate(item, playerPos, Quaternion.identity); //Creates a new instance of the item in the slot, spawning in the position specified by the playerPos variable, with zero rotation
+    }
+
+   
+}
