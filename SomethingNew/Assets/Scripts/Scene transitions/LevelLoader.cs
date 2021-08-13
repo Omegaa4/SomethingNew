5558af28bfb4ca523067270f70d6dab391ce4a02
BigPotato182 <78880474+BigPotato182@users.noreply.github.com>
2021-08-13 12:35:36 +1200

    Integrating scene transitions

--- SomethingNew/Assets/Scripts/Scene transitions/LevelLoader.cs
+++ SomethingNew/Assets/Scripts/Scene transitions/LevelLoader.cs
@@ -9,13 +9,9 @@ public class LevelLoader : MonoBehaviour
 
     public float transitionTime = 1f;
 
-    // Update is called once per frame
-    void Update()
-    {
-       if(Input.GetMouseButtonDown(0))
-       {
-            LoadNextLevel();
-       }
+    public void PlayGame()
+    {    
+       LoadNextLevel();
     }
 
     public void LoadNextLevel()
